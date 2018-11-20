2aa4da5b72a35f53ebcbe998af7ce7d6da0fb1f6
Alan Gomes Coimbra <algcoimbra01@gmail.com>
2018-11-20 18:03:47 -0200

    Consulta com Varios Criterios

--- src/main/java/com/workShopMongo/service/PostsService.java
+++ src/main/java/com/workShopMongo/service/PostsService.java
@@ -1,5 +1,6 @@
 package com.workShopMongo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,11 @@ public class PostsService {
 	
 	public List<Post> findyByTitle(String text){
 		return repo.serachTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
